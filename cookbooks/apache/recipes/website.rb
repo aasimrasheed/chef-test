a2148663dd4b60857503fb01a6a3e3e3bf2f4191
Chef User <chef.user@chefacademy.com>
2017-09-22 13:19:58 +0000

    Platform independent changes for Website Recipe

--- cookbooks/apache/recipes/website.rb
+++ cookbooks/apache/recipes/website.rb
@@ -1,6 +1,11 @@
 
+if node['platform_family'] == 'rhel'
+	var_path = '/var/www/html/index.html'
+elsif  node['platform_family'] == 'debian'
+        var_path = '/var/www/index.html'
+end
 
 file 'default www' do
-	path '/var/www/html/index.html'
+	path var_path
 	content 'H e l l o   W o r l d !!!!'
 end
