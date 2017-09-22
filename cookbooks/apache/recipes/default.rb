092dd9f34b290c5ebaec39643e700c97cc99d5fc
Chef User <chef.user@chefacademy.com>
2017-09-22 12:33:47 +0000

    Added MOTD recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,12 +5,20 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 #
+if node['platform_family'] == 'rhel'
+	var_package = 'httpd'
+elsif node['platform_family'] == 'debian'
+	var_package = 'apache2'
+end
+
 package 'apache' do
-	package_name 'httpd'
+	package_name var_package
 	action :install
 end
 
 service 'apache' do
-	service_name 'httpd'
+	service_name var_package
 	action [:enable, :start]
 end
+
+#include_recipe 'apache::website'
